9877d759c9b146d6c42d3386cb42a0820732ee3a
Xor Boole <mcyoung@mit.edu>
2014-12-23 22:07:21 -0400

    Fix SPIGOT-262: Entity metadata is cleared on entity death.

--- src/main/java/org/bukkit/craftbukkit/metadata/EntityMetadataStore.java
+++ src/main/java/org/bukkit/craftbukkit/metadata/EntityMetadataStore.java
@@ -20,4 +20,23 @@ public class EntityMetadataStore extends MetadataStoreBase<Entity> implements Me
     protected String disambiguate(Entity entity, String metadataKey) {
         return entity.getUniqueId().toString() + ":" + metadataKey;
     }
+
+    // Unfortunately, there's no way to track all of the metadata attached to an
+    // entity because apparently it was a good idea to combine the entity and
+    // metadata identifiers. This is the bad solution to the metadata framework
+    // being utterly terrible.
+
+    @Override
+    public synchronized void setMetadata(Entity subject, String metadataKey, org.bukkit.metadata.MetadataValue newMetadataValue) {
+        super.setMetadata(subject, metadataKey, newMetadataValue);
+        entityToKeys.put(subject.getUniqueId(), new Object[] { metadataKey, newMetadataValue.getOwningPlugin() });
+    }
+
+    private com.google.common.collect.Multimap<java.util.UUID, Object[]> entityToKeys = com.google.common.collect.HashMultimap.create();
+
+    public void clearMetadata(Entity entity) {
+        for(Object[] key : entityToKeys.removeAll(entity.getUniqueId())){
+            this.removeMetadata(entity, ((String) key[0]), ((org.bukkit.plugin.Plugin) key[1]));
+        }
+    }
 }
