649e5b9c1e67c731e886d1313f935bc0c083c7be
Phoenix616 <mail@moep.tv>
2017-04-21 12:48:03 +0100

    Do EnchantmentTest from the correct direction Previously this checked if a field existed for the enchantment name in the Enchantment class, now it checks if the field name matches the name of the assigned Enchantment

--- src/test/java/org/bukkit/EnchantmentTest.java
+++ src/test/java/org/bukkit/EnchantmentTest.java
@@ -11,14 +11,15 @@ import static org.junit.Assert.assertTrue;
 public class EnchantmentTest {
 
     @Test
-    public void verifyEnchantmentFields() {
-        for (Enchantment enchantment : Enchantment.values()) {
-            try {
-                Field enchantmentField = Enchantment.class.getField(enchantment.getName());
-                assertTrue("Field for " + enchantment.getName() + " in org.bukkit.enchantments.Enchantment is not public", Modifier.isPublic(enchantmentField.getModifiers()));
-                assertTrue("Field for " + enchantment.getName() + " in org.bukkit.enchantments.Enchantment is not static", Modifier.isStatic(enchantmentField.getModifiers()));
-            } catch (NoSuchFieldException e) {
-                throw new AssertionError("There is no public static field defined for the enchantment " + enchantment.getName() + " in org.bukkit.enchantments.Enchantment");
+    public void verifyEnchantmentNames() throws IllegalAccessException {
+        for (Field enchantField : Enchantment.class.getFields()) {
+            boolean isEnchantmentField = Enchantment.class.isAssignableFrom(enchantField.getType())
+                    && Modifier.isPublic(enchantField.getModifiers())
+                    && Modifier.isStatic(enchantField.getModifiers())
+                    && Modifier.isFinal(enchantField.getModifiers());
+            if (isEnchantmentField) {
+                Enchantment enchantment = (Enchantment) enchantField.get(Enchantment.class);
+                assertTrue("Name of enchantment " + enchantField.getName() + " in org.bukkit.enchantments.Enchantment does not match name returned by org.bukkit.craftbukkit.enchantments.CraftEnchantment.getName() (" + enchantment.getName() + ")", enchantField.getName().equals(enchantment.getName()));
             }
         }
     }
