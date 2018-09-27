b9d9782ee0af5933850567861dd39880e5962e47
Senmori <thesenmori@gmail.com>
2018-09-27 09:23:46 -0400

    Add isValidDoor(BlockData) method to check if a given BlockData is a valid door for Villages. Let 'addDoorsAround' add any new doors found automatically.

--- src/main/java/org/bukkit/craftbukkit/village/CraftVillageCollection.java
+++ src/main/java/org/bukkit/craftbukkit/village/CraftVillageCollection.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ImmutableList;
 import net.minecraft.server.BlockPosition;
 import net.minecraft.server.PersistentVillage;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.craftbukkit.block.data.CraftBlockData;
 import org.bukkit.util.Vector;
 import org.bukkit.village.Village;
 import org.bukkit.village.VillageCollection;
@@ -24,6 +27,11 @@ public class CraftVillageCollection implements VillageCollection {
     }
 
     @Override
+    public World getWorld() {
+        return getHandle().world.getWorld();
+    }
+
+    @Override
     public Collection<Village> getVillages() {
         ImmutableList.Builder<Village> bukkitVillages = new ImmutableList.Builder<>();
         getHandle().getVillages().forEach((v) -> bukkitVillages.add(v.bukkitVillage));
@@ -40,16 +48,22 @@ public class CraftVillageCollection implements VillageCollection {
     @Override
     public void addDoorsAroundLocation(Location location, Vector radius) {
         getHandle().addDoorsAround(new BlockPosition(location.getX(), location.getY(), location.getZ()), radius.getBlockX(), radius.getBlockY(), radius.getBlockZ());
+        getHandle().h(); // PAIL rename addNewDoors
     }
 
     @Override
     public VillageDoor getDoorAtLocation(Location location) {
-        net.minecraft.server.VillageDoor nms = getHandle().c(new BlockPosition(location.getX(), location.getY(), location.getZ()));
+        net.minecraft.server.VillageDoor nms = getHandle().c(new BlockPosition(location.getX(), location.getY(), location.getZ())); // PAIL rename getDoorAtPosition
         return nms == null ? null : nms.bukkitVillageDoor;
     }
 
     @Override
     public boolean isValidDoor(Location location) {
         return getHandle().a(getHandle().world.getType(new BlockPosition(location.getX(), location.getY(), location.getZ()))); // PAIL rename isValidDoor
+    }
+
+    @Override
+    public boolean isValidDoor(BlockData data) {
+        return getHandle().a(((CraftBlockData)data).getState());
     }
 }
