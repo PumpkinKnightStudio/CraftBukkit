096ad31c618724239186b5ae3d3446b5c075b8a3
MartenM <marten.struijk@gmail.com>
2022-05-17 17:09:18 +0200

    Implemented new API classes

---
+++ src/main/java/org/bukkit/craftbukkit/advancement/CraftAdvancementDisplay.java
@@ -0,0 +1,61 @@
+package org.bukkit.craftbukkit.advancement;
+
+import net.minecraft.advancements.AdvancementDisplay;
+import org.bukkit.advancement.AdvancementType;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.util.CraftChatMessage;
+import org.bukkit.inventory.ItemStack;
+
+public class CraftAdvancementDisplay implements org.bukkit.advancement.AdvancementDisplay {
+
+    private AdvancementDisplay handle;
+
+    public CraftAdvancementDisplay(AdvancementDisplay handle) {
+        this.handle = handle;
+    }
+
+    @Override
+    public String getTitle() {
+        return CraftChatMessage.fromComponent(handle.getTitle());
+    }
+
+    @Override
+    public String getDescription() {
+        return CraftChatMessage.fromComponent(handle.getDescription());
+    }
+
+    @Override
+    public ItemStack getIcon() {
+        return CraftItemStack.asBukkitCopy(handle.getIcon());
+    }
+
+    @Override
+    public boolean shouldShowToast() {
+        return handle.shouldShowToast();
+    }
+
+    @Override
+    public boolean shouldAnnounceChat() {
+        return handle.shouldAnnounceChat();
+    }
+
+    @Override
+    public boolean isHidden() {
+        return handle.isHidden();
+    }
+
+    @Override
+    public float getX() {
+        return handle.getX();
+    }
+
+    @Override
+    public float getY() {
+        return handle.getY();
+    }
+
+    @Override
+    public AdvancementType getType() {
+        return AdvancementType.byName(handle.getFrame().getName());
+    }
+}
