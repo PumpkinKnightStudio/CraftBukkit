509a9eb4f0a40768045f63cbb7fc91754efa6d25
Doc <nachito94@msn.com>
2022-06-08 23:37:38 -0400

    Implement more warden methods

--- src/main/java/org/bukkit/craftbukkit/entity/CraftWarden.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftWarden.java
@@ -1,10 +1,12 @@
 package org.bukkit.craftbukkit.entity;
 
 import com.google.common.base.Preconditions;
+import net.minecraft.world.entity.monster.warden.AngerLevel;
 import net.minecraft.world.entity.monster.warden.Warden;
 import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 
 public class CraftWarden extends CraftMonster implements org.bukkit.entity.Warden {
 
@@ -47,5 +49,27 @@ public class CraftWarden extends CraftMonster implements org.bukkit.entity.Warde
 
         getHandle().clearAnger(((CraftEntity) entity).getHandle());
         getHandle().getAngerManagement().increaseAnger(((CraftEntity) entity).getHandle(), anger);
+    }
+
+    @Override
+    public void clearAnger(Entity entity) {
+        Preconditions.checkArgument(entity != null, "Entity cannot be null");
+
+        getHandle().clearAnger(((CraftEntity) entity).getHandle());
+    }
+
+    @Override
+    public LivingEntity getEntityAngryAt() {
+        return (LivingEntity) getHandle().getEntityAngryAt().map(net.minecraft.world.entity.Entity::getBukkitEntity).orElse(null);
+    }
+
+    @Override
+    public LevelAnger getLevelAnger() {
+        AngerLevel angerLevel = getHandle().getAngerLevel();
+        return switch (angerLevel) {
+            case CALM -> LevelAnger.CALM;
+            case AGITATED -> LevelAnger.AGITATED;
+            case ANGRY -> LevelAnger.ANGRY;
+        };
     }
 }
