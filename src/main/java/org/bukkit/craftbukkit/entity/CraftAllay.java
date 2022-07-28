c5878d6fa60ad59377a08f2936075aa7ffb3072b
Doc <nachito94@msn.com>
2022-07-28 10:51:53 -0400

    Fix annotation

--- src/main/java/org/bukkit/craftbukkit/entity/CraftAllay.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftAllay.java
@@ -11,7 +11,6 @@ import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.inventory.CraftInventory;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.Inventory;
-import org.jetbrains.annotations.NotNull;
 
 public class CraftAllay extends CraftCreature implements org.bukkit.entity.Allay {
 
@@ -65,7 +64,7 @@ public class CraftAllay extends CraftCreature implements org.bukkit.entity.Allay
     }
 
     @Override
-    public void startDancing(@NotNull Location location) {
+    public void startDancing(Location location) {
         Preconditions.checkArgument(location != null, "Location cannot be null");
         Preconditions.checkArgument(location.getBlock().getType().equals(Material.JUKEBOX), "The Block in the Location need to be a JukeBox");
         getHandle().setJukeboxPlaying(new BlockPosition(location.getX(), location.getY(), location.getZ()), true);
