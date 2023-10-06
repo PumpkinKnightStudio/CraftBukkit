57402688b8ae4e9f27997361ab60c33e88ec4494
Mihad Khondker <mihadkho2019@gmail.com>
2023-10-06 01:04:54 -0400

    Patching ContainerEnchantTable, CraftInventoryEnchanting implements new interface

--- src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryEnchanting.java
+++ src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryEnchanting.java
@@ -1,12 +1,22 @@
 package org.bukkit.craftbukkit.inventory;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.IInventory;
+import net.minecraft.world.inventory.ContainerEnchantTable;
+import org.bukkit.NamespacedKey;
+import org.bukkit.craftbukkit.util.CraftNamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.EnchantingInventory;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CraftInventoryEnchanting extends CraftInventory implements EnchantingInventory {
-    public CraftInventoryEnchanting(IInventory inventory) {
+    private final ContainerEnchantTable container;
+    public CraftInventoryEnchanting(IInventory inventory, ContainerEnchantTable container) {
         super(inventory);
+        this.container = container;
     }
 
     @Override
@@ -27,5 +37,31 @@ public class CraftInventoryEnchanting extends CraftInventory implements Enchanti
     @Override
     public ItemStack getSecondary() {
         return getItem(1);
+    }
+
+    @Override
+    public int[] getEnchantmentCosts() {
+        return container.costs;
+    }
+
+    @Override
+    public List<Enchantment> getEnchantmentClues() {
+        List<Enchantment> enchantmentClues = new ArrayList<>();
+
+        for (int i=0; i<3; i++) {
+            if (container.levelClue[i]!=-1)
+                enchantmentClues.add(Enchantment.
+                        getByKey(CraftNamespacedKey
+                                .fromMinecraft(BuiltInRegistries.ENCHANTMENT
+                                        .getKey(net.minecraft.world.item.enchantment.Enchantment.byId(container.levelClue[i])))));
+            else enchantmentClues.add(null);
+        }
+
+        return enchantmentClues;
+    }
+
+    @Override
+    public int[] getLevelClues() {
+        return container.levelClue;
     }
 }
