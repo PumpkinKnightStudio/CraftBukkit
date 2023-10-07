41b52e6d2a8a3abd3ea6703ae3f95e722941723b
Mihad Khondker <mihadkho2019@gmail.com>
2023-10-07 03:47:19 -0400

    adjusted some methods and implemented setters

--- src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryEnchanting.java
+++ src/main/java/org/bukkit/craftbukkit/inventory/CraftInventoryEnchanting.java
@@ -1,16 +1,13 @@
 package org.bukkit.craftbukkit.inventory;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.IInventory;
 import net.minecraft.world.inventory.ContainerEnchantTable;
-import org.bukkit.NamespacedKey;
 import org.bukkit.craftbukkit.util.CraftNamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.EnchantingInventory;
 import org.bukkit.inventory.ItemStack;
-import org.jetbrains.annotations.Nullable;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CraftInventoryEnchanting extends CraftInventory implements EnchantingInventory {
     private final ContainerEnchantTable container;
@@ -40,24 +37,43 @@ public class CraftInventoryEnchanting extends CraftInventory implements Enchanti
     }
 
     @Override
+    public void setEnchantmentCosts(int[] costs) {
+        Preconditions.checkArgument(costs.length == 3, "There must be 3 slots for the enchantment costs. Leave slot as null if needed.");
+        System.arraycopy(costs, 0, container.costs, 0, 3);
+    }
+
+    @Override
     public int[] getEnchantmentCosts() {
         return container.costs;
     }
 
     @Override
-    public List<Enchantment> getEnchantmentClues() {
-        List<Enchantment> enchantmentClues = new ArrayList<>();
+    public void setEnchantmentClues(Enchantment[] enchantments) {
+        Preconditions.checkArgument(enchantments.length == 3, "There must be 3 slots for the enchantments. Leave slot as null if needed.");
+        for (int i=0; i<3; i++) {
+            if (enchantments[i] != null)
+                container.enchantClue[i] = BuiltInRegistries.ENCHANTMENT.getId(BuiltInRegistries.ENCHANTMENT.get(CraftNamespacedKey.toMinecraft(enchantments[i].getKey())));
+        }
+    }
+
+    @Override
+    public Enchantment[] getEnchantmentClues() {
+        Enchantment[] enchantmentClues = new Enchantment[3];
 
         for (int i=0; i<3; i++) {
-            if (container.levelClue[i]!=-1)
-                enchantmentClues.add(Enchantment.
-                        getByKey(CraftNamespacedKey
-                                .fromMinecraft(BuiltInRegistries.ENCHANTMENT
-                                        .getKey(net.minecraft.world.item.enchantment.Enchantment.byId(container.levelClue[i])))));
-            else enchantmentClues.add(null);
+            enchantmentClues[i] = container.levelClue[i]==-1 ? null
+            : Enchantment.getByKey(CraftNamespacedKey.fromMinecraft(BuiltInRegistries.ENCHANTMENT.getKey(net.minecraft.world.item.enchantment.Enchantment.byId(container.levelClue[i]))));
         }
 
         return enchantmentClues;
+    }
+
+    @Override
+    public void setLevelClues(int[] levels) {
+        Preconditions.checkArgument(levels.length == 3, "There must be 3 slots for the enchantment levels. Leave slot as -1 if want to omit.");
+        for (int i=0; i<3; i++) {
+            if (levels[i] != -1) container.levelClue[i] = levels[i];
+        }
     }
 
     @Override
